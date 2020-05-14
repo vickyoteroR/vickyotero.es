ab78fc861d1f5d34eabc4330c5dbbcba1d8a03e0
Vicky Otero <VickyOteroGithub@gmail.com>
2020-05-14 09:55:41 +0200

    header pug

--- public_html/js/vicky.js
+++ public_html/js/vicky.js
@@ -13,7 +13,7 @@ const nav_toggle = document.querySelector(nav_toggle_button);
 document.addEventListener("click", function(eventmobilenav) {
   if (!eventmobilenav.target.matches("#nav-toggle")) return;
   //if (!nav_toggle) return;
-  console.log(eventmobilenav);
+  //console.log(eventmobilenav);
   const main_nav_collapse = "#main-nav";
   const main_nav = document.querySelector(main_nav_collapse);
   main_nav.classList.toggle("collapse");
@@ -22,20 +22,24 @@ document.addEventListener("click", function(eventmobilenav) {
 //navbar PORTFOLIO
 
 const portfolio_tab_tag = ".porfolio .tab";
+console.log(portfolio_tab_tag);
+
 const portfolio_tab = document.querySelector(portfolio_tab_tag);
+console.log(portfolio_tab);
 document.addEventListener("click", function(eventportfolionav) {
   if (!eventportfolionav.target.matches(portfolio_tab)) return;
-  console.log(eventportfolionav);
+  // console.log(eventportfolionav);
 });
+
 //acordeón accesibilidad
 
 const barrier_label = document.querySelectorAll(".barrier").length;
 //console.log(barrier_label);
 [].forEach.call(document.querySelectorAll(".barrier"), function(eventaccordion) {
   eventaccordion.addEventListener("click", function() {
-    console.log(eventaccordion);
+    //console.log(eventaccordion);
     if (!barrier_label) return;
-    console.log(eventaccordion);
+    //console.log(eventaccordion);
 
     //eventaccordion.classList.add("on");
   });
